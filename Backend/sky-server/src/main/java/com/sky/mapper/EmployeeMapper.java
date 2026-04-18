b470f904f6afe126992009beb85c72852e042978
CCAramBAA <taopuying2002@gmail.com>
2026-04-18 19:07:28 +0800

    查询编辑员工信息

--- Backend/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ Backend/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -31,4 +31,13 @@ public interface EmployeeMapper {
      * @parame employee
      */
     void update(Employee employee);
+
+
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
