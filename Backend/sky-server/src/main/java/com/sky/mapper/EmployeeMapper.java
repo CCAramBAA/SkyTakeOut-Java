11958daf4a3ad497626550668e67252291414bbe
CCAramBAA <taopuying2002@gmail.com>
2026-04-19 08:57:16 +0800

    自动填充公共字段

--- Backend/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ Backend/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -21,6 +23,7 @@ public interface EmployeeMapper {
     /*
      *插入员工数据
      */
+    @AutoFill(value = OperationType.INSERT)
     @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) values (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
 
@@ -30,6 +33,7 @@ public interface EmployeeMapper {
      *根据主键动态修改属性
      * @parame employee
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
 
